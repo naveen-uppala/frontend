25617f4ba356fb7514077a83ec48233b4cf6256c
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2025-03-30 12:21:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post('/customer/customerDetails', async (req, res) => {
     try {
       const data = req.body;
       // const response = await axios.post('https://backend.hyderabad-packers-movers.in/customer/customerDetails', data);
-      const response = await axios.post('http://3.251.65.164:8080/customer/customerDetails', data);
+      const response = await axios.post('http://54.246.33.248:8080/customer/customerDetails', data);
       // Send the JSON response from Spring Boot to the frontend
       res.status(200).json(response.data);
     } catch (error) {
