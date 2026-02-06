f2de948afd15910234fe075cac6812d8114abb60
Naveen Uppala <36377958+naveen-uppala@users.noreply.github.com>
2026-02-06 17:54:43 +0530

    Update juice-shop-auth.js

--- .github/workflows/zap/scripts/juice-shop-auth.js
+++ .github/workflows/zap/scripts/juice-shop-auth.js
@@ -1,28 +1,86 @@
-function authenticate(helper, paramsValues, credentials) {
+env:
+  contexts:
+    - name: juice-shop-context
+      urls:
+        - https://juice-shop.herokuapp.com
+      includePaths:
+        - https://juice-shop.herokuapp.com/.*
 
-    var loginUrl = "https://juice-shop.herokuapp.com/rest/user/login";
+      authentication:
+        method: script
+        parameters:
+          script: juice-shop-auth
 
-    var body =
-        JSON.stringify({
-            email: credentials.getParam("username"),
-            password: credentials.getParam("password")
-        });
+      sessionManagement:
+        method: cookie
 
-    var msg = helper.prepareMessage();
-    msg.setRequestHeader(
-        "POST " + loginUrl + " HTTP/1.1\r\n" +
-        "Content-Type: application/json\r\n"
-    );
-    msg.setRequestBody(body);
-    msg.getRequestHeader().setContentLength(body.length);
+      users:
+        - name: admin-user
+          credentials:
+            username: admin@juice-sh.op
+            password: admin123
 
-    helper.sendAndReceive(msg);
+# 🔐 INLINE AUTH SCRIPT (NO FILE PATH ISSUES)
+scripts:
+  - name: juice-shop-auth
+    type: authentication
+    engine: ECMAScript
+    scriptInline: |
+      function authenticate(helper, paramsValues, credentials) {
 
-    // 🔴 HARD FAIL if auth cookie is missing
-    var cookies = msg.getResponseHeader().getHeaderValues("Set-Cookie");
-    if (cookies == null || cookies.toString().indexOf("token=") === -1) {
-        throw "Authentication failed: token cookie not found";
-    }
+          var loginUrl = "https://juice-shop.herokuapp.com/rest/user/login";
 
-    return msg;
-}
+          var body = JSON.stringify({
+              email: credentials.getParam("username"),
+              password: credentials.getParam("password")
+          });
+
+          var msg = helper.prepareMessage();
+          msg.setRequestHeader(
+              "POST " + loginUrl + " HTTP/1.1\r\n" +
+              "Content-Type: application/json\r\n"
+          );
+          msg.setRequestBody(body);
+          msg.getRequestHeader().setContentLength(body.length);
+
+          helper.sendAndReceive(msg);
+
+          // 🔴 FAIL HARD if authentication token is missing
+          var cookies = msg.getResponseHeader().getHeaderValues("Set-Cookie");
+          if (cookies == null || cookies.toString().indexOf("token=") === -1) {
+              throw "Authentication failed: token cookie not found";
+          }
+
+          return msg;
+      }
+
+jobs:
+  - type: spider
+    parameters:
+      context: juice-shop-context
+      user: admin-user
+      maxDuration: 5
+
+  - type: activeScan
+    parameters:
+      context: juice-shop-context
+      user: admin-user
+      maxScanDurationInMins: 5
+
+  - type: report
+    parameters:
+      template: traditional-html
+      reportDir: .
+      reportFile: report_html.html
+
+  - type: report
+    parameters:
+      template: traditional-json
+      reportDir: .
+      reportFile: report_json.json
+
+  - type: report
+    parameters:
+      template: traditional-md
+      reportDir: .
+      reportFile: report_md.md
